21dae308b7cf1f77b6dded4601e083a5fcd612b1
Sujit Shakya <sshakya.mail@gmail.com>
2017-12-18 17:15:57 +0545

    Fix the cloud go_url issue

--- lib/http_service/naviai.rb
+++ lib/http_service/naviai.rb
@@ -1,17 +1,14 @@
 require "httparty"
 
-GO_SERVER_URL = 'http://localhost:9090/v2/metas'
-
 module HTTPService
   class NaviAI
 
     def self.start(file_path, client_type, token)
-      if client_type == 'local'
-        go_url = 'http://localhost:9090'
-      else
-        go_url = '34.214.134.104:9090'
+      go_url = 'http://localhost:9090/v2/metas'
+      if client_type == 'cloud'
+        go_url = '34.214.134.104:9090/v2/metas'
       end
-      HTTParty.post(GO_SERVER_URL, body: { client_type: client_type, list_meta_path: file_path, token: token }.to_json)
+      HTTParty.post(go_url, body: { client_type: client_type, list_meta_path: file_path, token: token }.to_json)
     end
   end
 end

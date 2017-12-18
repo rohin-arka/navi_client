98bfc8e14e38ecf454017e6cb76adbb12a3390e0
Sujit Shakya <sshakya.mail@gmail.com>
2017-12-18 17:28:08 +0545

    Fix the Bad URI error

--- lib/http_service/naviai.rb
+++ lib/http_service/naviai.rb
@@ -6,7 +6,7 @@ module HTTPService
     def self.start(file_path, client_type, token)
       go_url = 'http://localhost:9090/v2/metas'
       if client_type == 'cloud'
-        go_url = '34.214.134.104:9090/v2/metas'
+        go_url = 'http://34.214.134.104:9090/v2/metas'
       end
       HTTParty.post(go_url, body: { client_type: client_type, list_meta_path: file_path, token: token }.to_json)
     end
